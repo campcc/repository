310befea87768addf8a725135a30990a544dee52
campcc <1210131782@qq.com>
2020-06-18 11:12:05 +0800

    chore: update site title

--- .umirc.ts
+++ .umirc.ts
@@ -4,7 +4,7 @@ export default defineConfig({
   base: '/repository/',
   publicPath: '/repository/',
   exportStatic: {},
-  title: 'repository',
+  title: 'FED Repository',
   favicon:
     'https://user-images.githubusercontent.com/9554297/83762004-a0761b00-a6a9-11ea-83b4-9c8ff721d4b8.png',
   logo:
